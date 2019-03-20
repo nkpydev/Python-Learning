56e0fae9d5e1ebbc918f82b78d8284ca18ea1680
Nitin K <39520442+nkpydev@users.noreply.github.com>
2019-03-20 18:24:02 +0530

    Commit

---
+++ List/print_list_items_as_individual_seperated_by_comma.py
@@ -0,0 +1,5 @@
+
+if __name__ == '__main__':
+    name_list = ['Tim', 'Steve', 'Larry', 'Sergey']
+    print(name_list) # This will give you ['','','','']
+    print(','.join(name_list)) # This will give you only name items inside the list seperated by ,
---
+++ List/user_input_into_list_example.py
@@ -0,0 +1,4 @@
+
+if __name__ == '__main__':
+    user_input = [example for example in input('\nExample of User Input to be taken into List ["," seperated]:\t').split(',')]
+    print(user_input)
