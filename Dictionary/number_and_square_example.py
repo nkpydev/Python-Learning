d0e468bdb76a4d636cdb7e00c96448aecf5d7833
Nitin K <39520442+nkpydev@users.noreply.github.com>
2019-04-08 20:40:30 +0530

    Commit

--- Dictionary/number_and_square_example.py
+++ Dictionary/number_and_square_example.py
@@ -1,11 +1,21 @@
 # We want the Output to be a Dict - {1:1,2:4,3:9...}
-# We also want to use the LAMBDA Function
+def basic_method(user_input):
+    sq_of_number = lambda num: num*num
+    result_dict_1 = {}
+    for x in user_input:
+        result_dict_1[x] = sq_of_number(x)
+    print(result_dict_1)
 
-user_input = [1,2,3,4,5,6,7,8,9,10]
-sq_of_number = lambda num: num*num
-result_dict = {}
+def pythonic_simple_method(user_input):
+    result_dict_2 = {x:x*x for x in user_input}
+    print(result_dict_2)
 
-
-for x in user_input:
-    result_dict[x] = sq_of_number(x)
-print(result_dict)
+if __name__ == "__main__":
+    user_input = [1,2,3,4,5,6,7,8,9,10]
+    user_choice = int(input('\nEnter your Choice: 1] Basic Method\t2] Pythonic Simple Method:\t'))
+    if user_choice == 1:
+        basic_method(user_input)
+    elif user_choice == 2:
+        pythonic_simple_method(user_input)
+    else:
+        print('Wrong Choice!!')
