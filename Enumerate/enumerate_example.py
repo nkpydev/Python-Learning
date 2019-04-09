f0f51df3b782f3f8cee01434a13fa2afaa7958d0
Nitin K <39520442+nkpydev@users.noreply.github.com>
2019-04-09 15:01:35 +0530

    Commit

---
+++ Enumerate/enumerate_example.py
@@ -0,0 +1,9 @@
+weekdays = [
+            'Monday', 'Tuesday',
+            'Wednesday', 'Thursday',
+            'Friday', 'Saturday',
+            'Sunday'
+            ]
+
+for i, day in enumerate(weekdays):
+    print('{} is weekday {}'.format(day,i))
