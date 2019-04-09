da41311a496e527e922b8410c2895f476ff8feff
Nitin K <39520442+nkpydev@users.noreply.github.com>
2019-04-09 15:09:38 +0530

    Commit

--- List/user_input_into_list_example.py
+++ List/user_input_into_list_example.py
@@ -1,3 +1,4 @@
+# This is example of List Comprehension.
 
 if __name__ == '__main__':
     user_input = [example for example in input('\nExample of User Input to be taken into List ["," seperated]:\t').split(',')]
